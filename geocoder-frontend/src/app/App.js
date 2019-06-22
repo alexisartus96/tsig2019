1adfedfbeef9a0ce2b3926e95f3798dbd49c0a14
Federico Del Río <fededelriosilva@gmail.com>
2019-06-22 12:22:19 -0300

    Remove unnecessary 'home' folder.

--- geocoder-frontend/src/app/App.js
+++ geocoder-frontend/src/app/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Element } from 'react-scroll';
 import MapView from './map/components/MapContainer';
-import global from './home/css/global.css'; // eslint-disable-line
 
 const App = () => (
   <div>
