df6aba50b9c3c02c263e339adb39e7c381f9f2cc
Federico Del Río <fededelriosilva@gmail.com>
2019-06-22 15:05:57 -0300

    Move XMLs to 'utils' folder.

--- geocoder-frontend/src/app/map/components/MapContainer.js
+++ geocoder-frontend/src/app/map/components/MapContainer.js
@@ -10,6 +10,7 @@ import SubBoxText from '../styles/SubBoxText';
 import MapViewComponent from '../styles/MapViewComponent';
 import ToggleMenu from '../styles/ToggleMenu';
 import { InputName, InputNumber, InputRadioButton, LabelRadioButton, SearchButton, SearchNav, SearchNavSubBox, InputGroup } from '../styles/SearchNav';
+import { XMLGeocoder, XMLGeocoderCruce, XMLGeocoderInversa } from '../../utils/xml';
 
 const Map = ReactMapboxGl({
   accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
@@ -38,77 +39,6 @@ function Toggle() {
     showMenu: !this.state.showMenu,
   });
 }
-
-const XMLGeocoder = (street, number) => (`<?xml version="1.0" encoding="UTF-8"?>
-  <wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
-    <ows:Identifier>gs:TSIGEGeocoder</ows:Identifier>
-    <wps:DataInputs>
-      <wps:Input>
-        <ows:Identifier>calle</ows:Identifier>
-      <wps:Data>
-          <wps:LiteralData>${street}</wps:LiteralData>
-        </wps:Data>
-      </wps:Input>
-      <wps:Input>
-        <ows:Identifier>numero</ows:Identifier>
-        <wps:Data>
-          <wps:LiteralData>${number}</wps:LiteralData>
-        </wps:Data>
-      </wps:Input>
-    </wps:DataInputs>
-    <wps:ResponseForm>
-      <wps:RawDataOutput mimeType="application/gml-3.1.1">
-        <ows:Identifier>result</ows:Identifier>
-      </wps:RawDataOutput>
-    </wps:ResponseForm>
-  </wps:Execute>`);
-
-const XMLGeocoderCruce = (street, corner) => (`<?xml version="1.0" encoding="UTF-8"?>
-  <wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
-    <ows:Identifier>gs:TSIGEGeocoderCruce</ows:Identifier>
-    <wps:DataInputs>
-      <wps:Input>
-        <ows:Identifier>calle</ows:Identifier>
-        <wps:Data>
-          <wps:LiteralData>${street}</wps:LiteralData>
-        </wps:Data>
-      </wps:Input>
-      <wps:Input>
-        <ows:Identifier>esquina</ows:Identifier>
-        <wps:Data>
-          <wps:LiteralData>${corner}</wps:LiteralData>
-        </wps:Data>
-      </wps:Input>
-    </wps:DataInputs>
-    <wps:ResponseForm>
-      <wps:RawDataOutput>
-        <ows:Identifier>result</ows:Identifier>
-      </wps:RawDataOutput>
-    </wps:ResponseForm>
-  </wps:Execute>`);
-
-const XMLGeocoderInversa = (lat, lon) => (`<?xml version="1.0" encoding="UTF-8"?><wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
-  <ows:Identifier>gs:TSIGEGeocoderInverso</ows:Identifier>
-  <wps:DataInputs>
-    <wps:Input>
-      <ows:Identifier>longitud</ows:Identifier>
-      <wps:Data>
-        <wps:LiteralData>${lon}</wps:LiteralData>
-      </wps:Data>
-    </wps:Input>
-    <wps:Input>
-      <ows:Identifier>latitud</ows:Identifier>
-      <wps:Data>
-        <wps:LiteralData>${lat}</wps:LiteralData>
-      </wps:Data>
-    </wps:Input>
-  </wps:DataInputs>
-  <wps:ResponseForm>
-    <wps:RawDataOutput>
-      <ows:Identifier>result</ows:Identifier>
-    </wps:RawDataOutput>
-  </wps:ResponseForm>
-  </wps:Execute>`);
 
 class MapContainer extends Component {
   constructor(props) {
