1c0412f67d64b4ec70bb075ab8c944b009b9396e
Federico Del Río <fededelriosilva@gmail.com>
2019-06-23 10:19:18 -0300

    Add 'Candara' font again.

--- geocoder-frontend/src/app/App.js
+++ geocoder-frontend/src/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Element } from 'react-scroll';
 import MapContainer from './map/components/MapContainer';
+import global from './map/css/global.css'; // eslint-disable-line
 
 const App = () => (
   <div>
--- geocoder-frontend/src/index.js
+++ geocoder-frontend/src/index.js
@@ -3,9 +3,6 @@ import { render } from 'react-dom';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
-document.body.style.margin = "0px";
-document.body.style.padding = "0px";
-
 render(
   <App />,
   document.getElementById('root'),
